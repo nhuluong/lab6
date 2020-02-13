a9936558b1a487026b26b570c11637fd1c49dc0f
Nhu Luong <nhuluong@Nhus-MacBook-Pro.local>
2020-02-13 00:05:57 -0800

    first commit

---
+++ public/js/facebook.js
@@ -0,0 +1,18 @@
+function checkLoginState()	{
+	FB.getLoginStatus(function(response)	{
+		statusChangeCallback(response);
+	});
+}
+
+function statusChangeCallback(response)	{
+	if (response.status === 'connected')	{
+		console.log('Successfully logged in with Facebook');
+		FB.api('/me?fields=name, first_name,picture.width(480)', changeUser);
+	}
+}
+
+//Add this callback at bottom of facebook.js and add the required functionality in it 
+function changeUser(response) {
+  //Add code to change name and image 
+  
+}
